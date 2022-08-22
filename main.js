d5a7dfb84fc37fea8a3ad9056ffa14349ddff6ac
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2022-08-22 14:30:14 -0500

    Update

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el")
 
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push("www.awesomelead.com")
+  myLeads.push(inputEl.value)
   console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.textContent += myLeads[i] + " ";
+}
