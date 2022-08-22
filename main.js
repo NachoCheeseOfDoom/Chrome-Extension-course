6db754b681118f441a4913b8b18042edbf65cd56
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2022-08-22 15:44:20 -0500

    Update

--- main.js
+++ main.js
@@ -2,16 +2,31 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el")
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+
+
+let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
   console.log(myLeads)
+  inputEl.value = '';
+  renderLeads();
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-  //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-  const li = document.createElement("li")
-  li.textContent = myLeads[i];
-  ulEl.append(li)
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+    listItems += `
+    <li>
+        <a target='_blank' href=' 
+          ${myLeads[i]}'> 
+          ${myLeads[i]} 
+        </a>
+    </li>`;
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i];
+    // ulEl.append(li)
+  }
+  ulEl.innerHTML = listItems;
 }
